14ce4d4740f8e2872dcd0d1641298962b7e9b055
jane <646829474@qq.com>
2016-12-09 10:55:04 +0800

    给listview设置adapter

--- app/src/main/java/com/gudongguangfo/listviewdiffitems/MainActivity.java
+++ app/src/main/java/com/gudongguangfo/listviewdiffitems/MainActivity.java
@@ -7,16 +7,14 @@ import android.widget.ListView;
 public class MainActivity extends AppCompatActivity {
 
     private ListView mListview;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mListview = (ListView) findViewById(R.id.lv);
-
         String[] contectName = {"张三", "李四", "王五", "啊啊"};
         int[] photoid= {R.drawable.bee,R.drawable.cat,R.drawable.pig,R.drawable.woniu};
-
+        //创建需要的adapter
         ManyItemTypesAdapter adapter = new ManyItemTypesAdapter(this,contectName,photoid);
         mListview.setAdapter(adapter);
 
